c9f33ecbcef54ba81167a64f07c3556bd3f4baa6
Michael Strunk <mmstrunk@Michaels-MacBook-Pro.local>
2013-01-31 20:18:12 -0500

    json project 4

---
+++ Desktop/Project4/JS/json.js
@@ -0,0 +1,49 @@
+// Michael Strunk VFW 0113 Json
+
+var json = {
+		"Item1": {
+			"visited": ["Late Time Visited Restaurant:", "1-2 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Mcdonalds"],
+			"favRest":	["What is your Favorite Restaurant:", "Apple Bees"],
+			"mostVis": ["Most visited Restaurant:", "Olive Garden"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "18%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Michael"],
+			"lName":	["Last Name", "Strunk"],
+			"quantity":	["Quantity:", "400"],
+			"details":	["Details:", "Good service is what i base my restaurant visits too."]	
+			
+		},
+		"Item2": {
+			"visited": ["Late Time Visited Restaurant:", "Last week"],
+			"lastName": ["Name of last visited Restaurant:", "burgerking"],
+			"favRest":	["What is your Favorite Restaurant:", "olive garden"],
+			"mostVis": ["Most visited Restaurant:", "mccabes"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "20%"],
+			"server":	["Check if food effects server tip.", "Yes"],
+			"fName":	["First Name", "Gary"],
+			"lName":	["Last Name", "Sheldon"],
+			"quantity":	["Quantity:", "650"],
+			"details":	["Details:", "I eat out way to much."]	
+			
+		},
+		"Item3": {
+			"visited": ["Late Time Visited Restaurant:", "3-4 Days"],
+			"lastName": ["Name of last visited Restaurant:", "Chillis"],
+			"favRest":	["What is your Favorite Restaurant:", "Outback"],
+			"mostVis": ["Most visited Restaurant:", "Outback"],
+			"alcohol":	["Do you order Alcohol beverages?", "Yes"],
+			"tip":	["General % tip", "12%"],
+			"server":	["Check if food effects server tip.", "No"],
+			"fName":	["First Name", "Teresa"],
+			"lName":	["Last Name", "Strunk"],
+			"quantity":	["Quantity:", "800"],
+			"details":	["Details:", "I love outbacks Green Beens"]	
+			
+		},
+
+}
+
+
